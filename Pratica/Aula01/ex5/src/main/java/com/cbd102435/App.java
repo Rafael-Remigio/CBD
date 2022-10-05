0c73784e93d00bc3c64bd638da959e59236f68ef
rafael <rafael.lixa@ua.pt>
2022-10-05 21:03:54 +0100

    Ex5 completed

--- Pratica/Aula01/ex5/src/main/java/com/cbd102435/App.java
+++ Pratica/Aula01/ex5/src/main/java/com/cbd102435/App.java
@@ -21,19 +21,64 @@ public class App
 
         String input = "";
 
-        while (!input.equals("6")) {
-            System.out.println("1.) Make a friend.");
+        while (!input.equals("exit")) {
+            System.out.println("\nChoose a number");
+            System.out.println("1.) Follow a friend.");
             System.out.println("2.) Send a message to another user");
             System.out.println("3.) Read messages");
             System.out.println("4.) Make a post");
-            System.out.println("5.) See posts from user");
+            System.out.println("5.) See posts from people you follow");
             System.out.println("6.) See all users ");
+            System.out.println("7.) List people I follow");
+            System.out.println("8.) Unfollow a friend.");
+            System.out.println("9.) Exit");
 
             input = scn.nextLine();
 
-            switch 
-
-
+            switch(input){
+                case "1":
+                    System.out.print("Who do you want to follow?: ");
+                    String newfriend = scn.nextLine();
+                    user.Befriend(newfriend);
+                    break;
+                case "2":
+                    System.out.print("Who we texting?: ");
+                    String person = scn.nextLine();
+                    System.out.print("And what are we texting?: ");
+                    String text = scn.nextLine();
+                    user.sendMsg(person, text);
+                    break;
+                case "3":
+                    user.Readmessages();
+                    break;
+                case "4":
+                    System.out.print("What are we posting about, what is on your mind?: ");
+                    String post = scn.nextLine();
+                    user.makePost(post);
+                    break;
+                case "5":
+                    user.ReadPosts();
+                    break;
+                case "6":
+                    user.listAllUsers();
+                    break;
+                case "7":
+                    user.peopleIfollow();   
+                    break;
+                case "8":
+                    System.out.print("Who do you want to unfollow?: ");
+                    String lostfriend = scn.nextLine();
+                    user.Unfriend(lostfriend);   
+                    break;
+                case "9":
+                    System.out.println("Bye see you tomorrow");
+                    input = "exit";
+                    break;
+                default: 
+                    System.out.println("No such option pls try again");
+            }
         }
+        scn.close();
+
     }
 }
--- Pratica/Aula01/ex5/src/main/java/com/cbd102435/Client.java
+++ Pratica/Aula01/ex5/src/main/java/com/cbd102435/Client.java
@@ -1,8 +1,10 @@
 package com.cbd102435;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import javax.swing.text.Style;
 
 import redis.clients.jedis.Jedis;
 
@@ -10,12 +12,27 @@ public class Client {
 
     Jedis jedis = new Jedis();
     String name;
-    ArrayList<String> following;
+    HashSet<String> following;
 
     Client(String name){
         this.name = name;
-        following = new ArrayList<String>();
+        following = new HashSet<String>();
         jedis.sadd("users",name);
+    }
+
+
+    public void makePost(String text) {
+        jedis.lpush(name+":posts", text);
+        System.out.println("Post was created and posted successfuly");
+    }
+
+    public void ReadPosts() {
+        for (String string : following) {
+            List<String> posts = jedis.lrange(string+":posts",0,-1);
+            for (String post : posts) {
+                System.out.println(string + ") "+post);
+            }
+        }
     }
 
     public void listAllUsers(){
@@ -25,15 +42,46 @@ public class Client {
         }
     }
 
-    public void sendMsg(String user,String Message){
+    public void Readmessages(){
+        long leng = jedis.llen(name+":Actualmessage");
+        while (leng > 0){
+            String message = jedis.rpop(name+":Actualmessage");
+            String person = jedis.rpop(name+":messageSender");
+            System.out.println(person + ") " + message);
+            leng -=1;
+        }
 
+    }
+
+    public void sendMsg(String user,String Message){
+        jedis.lpush(user+":Actualmessage", Message);
+        jedis.lpush(user+":messageSender", this.name);
+        System.out.println("Message was sent to " + user);
     }
 
     public void Befriend (String user){
         following.add(user);
-    }
-    public void Unfriend (String user){
-        following.remove(user);
+        jedis.sadd(name+":following",user);
+        System.out.println("Started following " + user);
+
     }
 
+    public void Unfriend (String user){
+        following.remove(user);
+        if (jedis.sismember( name+":following", user)){
+
+            jedis.srem(name+":following", user);
+            System.out.println("Stoped following " + user);
+        }
+        else {
+            System.out.println("You are not even following "+ user+"\nWhat are u doing?");
+        }
+    }
+
+    public void peopleIfollow(){
+        System.out.println("I follow:");
+        for (String string : following) {
+            System.out.println(string);
+        }
+    }
 }

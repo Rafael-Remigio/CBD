cf04a8d37b76c2ee8211daf6bd108dfbd64df79f
rafael <rafael.lixa@ua.pt>
2022-10-23 21:23:29 +0100

    changes to cat

--- Pratica/Aula02/2_5/InsertingCats/generateCats.py
+++ Pratica/Aula02/2_5/InsertingCats/generateCats.py
@@ -24,6 +24,7 @@ def __main__(numberOfCats):
     catFood = ["Complete Essentials Chicken & Rice Formula","Complete Essentials Chicken & Egg Formula","Hairball Management Chicken & Rice Formula","Indoor Salmon & Rice Formula","Indoor Turkey & Rice Formula","Sensitive Skin & Stomach Turkey & Oat Meal Formula","Sensitive Skin & Stomach Lamb & Rice Formula","Shredded Blend Indoor Turkey & Rice Formula","Development Chicken Entrée Grain Free Classic Wet Cat Food","Development Chicken & Liver Entrée Classic Wet Cat Food"]
     minimumCatWeigth = 2
     maxCatWeigth = 9
+    diseases = {"Kidney Disease": ["Frequent urination","Weight loss","Drinking a lot of water"],"Dental Issues": ["Gingivitis","Periodontitis","bleeding gums"],"Diabetes": ["Weight loss","Dehydration","Change in appetite","Sweet-smelling breath"],"Feline Immunodeficiency Virus": ["Gingivitis","Poor coat condition","Fever"],"Feline Leukemia Virus": ["Seizures","Diarrhea","Weight loss","Fever"],"Cancer": ["Weight loss","Anorexia","Diarrhea"],"Obesity": [],"Pancreatitis":["Vomiting","Fever","Diarrhea","Abdominal pain"],"Hyperthyroidism":["Weight loss","Increased appetite"],"Broken Bones": [],"High-Rise Syndrome": [],"Fleas": ["Scratching","hair loss"],"Tapeworms":[],"Eye Problems":["Dizziness"],"Heartworms": ["Weight Loss","Lethargy"],"Rabies": ["Fever"],"Ringworm": ["Scratching"],"Constipation": ["Bloody feces","Change in appetite"],"Food Allergies": [],"Feline Lower Urinary Tract Diseases (FLUTD)": ["Pain","Blood in Urine","Frequent urination"]}
 
     #People names
     f = open('firstNames.txt', 'r+')
@@ -61,6 +62,18 @@ def __main__(numberOfCats):
             pho_no = pho_no + str(random.randint(0, 9))
         catOwner["phone"] = pho_no
 
+        mycat["diseases"] = []
+        for i in range(3):
+            d = random.randint(0,len(diseases) * 2) 
+            if d >= len(diseases):
+                break
+            disease = {}
+            lista = list(diseases.keys())
+            disease["nome"] = lista[d]
+            disease["discoveryYear"] = random.randrange(int(mycat["birthYear"]),2022)
+            disease["symptoms"] = diseases[lista[d]]
+
+            mycat["diseases"].append(disease)
 
         mycat["owner"] = catOwner
         
